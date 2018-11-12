eceda864ee7495772c0277dc286c72b1f867b105
Izaskun Mallona <izaskun.mallona@gmail.com>
2018-11-12 16:01:08 +0100

    Adding scoring and segmentation calls as functions

--- nucleosome_shadows.sh
+++ nucleosome_shadows.sh
@@ -20,79 +20,116 @@ NTHREADS=12
 mkdir -p $WD/data
 cd $_
 
-# samtools index SRR299053_bwameth_default.bam
-# samtools view -h SRR299053_bwameth_default.bam chr14 | samtools view -BS - > chr14.bam
+function get_data {
+    wget https://github.com/PeteHaitch/methtuple/blob/master/data/pe_directional_1.fq.gz_bismark_bt2_pe.bam?raw=true \
+         --output-document pe_bt2.bam
 
-# run methtuple
+    wget https://github.com/PeteHaitch/methtuple/blob/master/data/se_directional.fq.gz_bismark_bt2.bam?raw=true \
+         --output-document se_bt2.bam
 
-wget https://github.com/PeteHaitch/methtuple/blob/master/data/pe_directional_1.fq.gz_bismark_bt2_pe.bam?raw=true \
-     --output-document pe_bt2.bam
+    samtools sort pe_bt2.bam  > pe_sorted.bam
+    # mv -f sorted.bam pe_bt2.bam
+    samtools index pe_sorted.bam
 
-wget https://github.com/PeteHaitch/methtuple/blob/master/data/se_directional.fq.gz_bismark_bt2.bam?raw=true \
-     --output-document se_bt2.bam
+    samtools view -H pe_bt2.bam  | fgrep PG
 
-samtools sort pe_bt2.bam  > pe_sorted.bam
-# mv -f sorted.bam pe_bt2.bam
-samtools index pe_sorted.bam
+    # ln -s /home/sorjuela/ScienceCloud/test1_pe.bam
+    # samtools view -s 0.05 -b test1_pe.bam > sorjuela.bam
+}
 
-samtools view -H pe_bt2.bam  | fgrep PG
 
-ln -s /home/sorjuela/ScienceCloud/test1_pe.bam
-samtools view -s 0.05 -b test1_pe.bam > sorjuela.bam
-
-bam=sorjuela.bam
-
-source $VIRTENVS/methtuple/bin/activate
-
-methtuple -m 2 --methylation-type CG $bam
-
-deactivate
-
+## @param the path to the bismark bamfile
+## output writes a methtuple output
 ## methtuple outputs are
 # chr	strand	pos1	pos2	MM	MU	UM	UU
 # 1      2       3       4      5       6      7        8
+function run_methtuple {
+    bam="$1 "
+    
+    source $VIRTENVS/methtuple/bin/activate
 
-# fn=pe_bt2.CG.2.tsv
-fn=sorjuela.CG.2.tsv
+    methtuple -m 2 --methylation-type CG $bam
 
-sed '1d' $fn | \
-    awk 'BEGIN {OFS=FS="\t"}{
+    deactivate
+}
+
+## parses run_methtupleoutputs to get meth agreement scores
+## @param the methtuple path
+## output writes a bed6 (score column goes to 0 to 1000)
+
+function get_agreement_score {
+    fn="$1"
+    sed '1d' $fn | \
+
+        awk 'BEGIN {OFS=FS="\t"}{
       print $1,$3,$4,"MM"$5";MU"$6";UM"$7";UU"$8,$5/($5+$6+$7+$8)*1000,$2
      }' | "$BEDTOOLS" sort > "$(basename $fn .tsv)"_scored.bed
+}
 
-
-sed '1d' $fn | \
-    awk 'BEGIN {OFS=FS="\t"}{
+## parses run_methtupleoutputs to get meth betavalues
+## @param the methtuple path
+## output writes a bed6 (score column goes to 0 to 1000)
+function get_methylation {
+    fn="$1"
+    
+    sed '1d' $fn | \
+        awk 'BEGIN {OFS=FS="\t"}{
       print $1,$3,$4,"MM"$5";MU"$6";UM"$7";UU"$8,((2*$5)+$6+$7)/(2*($5+$6+$7+$8))*1000,$2
-     }' | "$BEDTOOLS" sort > "$(basename $fn .tsv)"_meth.bed
+     }' | "$BEDTOOLS" sort > "$(basename $fn .tsv)"_meth.bed    
+}
 
-## to call metilene the scores should be like betavalues (from 0 to 1000)
-## so divide by 1000
+function prepare_metilene_input {
+    agreement_score_fn="$1"
+    background_score_fn="$2"
+    sample_name=$3
 
-## check the cpg offset, whether 0 or 1! setting up the middle point here
+    ## to call metilene the scores should be like betavalues (from 0 to 1000)
+    ## so divide by 1000
 
-# for bed in  "$(basename $fn .tsv)"_scored.bed  "$(basename $fn .tsv)"_meth.bed
-# do
-#     awk '{OFS=FS="\t"; print $1, int($2+($3-$2)/2), $5/1000}' $bed > \
-#         "$(basename $bed .be)"_metilene.input    
-# done
+    ## check the cpg offset, whether 0 or 1! setting up the middle point here
 
-## rather, using starts and ends and NOT merging strands @todo
-for bed in  "$(basename $fn .tsv)"_scored.bed  "$(basename $fn .tsv)"_meth.bed
-do
-    awk '{OFS=FS="\t"; print $1,$2,$3,$5/1000}' $bed > \
-        "$(basename $bed .be)"_metilene.input    
-done
+    # for bed in  "$(basename $fn .tsv)"_scored.bed  "$(basename $fn .tsv)"_meth.bed
+    # do
+    #     awk '{OFS=FS="\t"; print $1, int($2+($3-$2)/2), $5/1000}' $bed > \
+    #         "$(basename $bed .be)"_metilene.input    
+    # done
+
+    ## rather, using starts and ends and NOT merging strands @todo
+    
+    ## rather, using starts and ends and NOT merging strands @todo
+    for bed in $agreement_score_fn $background_score_fn
+    do
+        awk '{OFS=FS="\t"; print $1,$2,$3,$5/1000}' $bed > \
+            "$bed"_metilene.input    
+    done
+
+    perl ${METILENE_INPUT} -in1 "$agreement_score_fn"_metilene.input \
+         -in2  "$background_score_fn"_metilene.input \
+         -out "$sample_name"_metilene \
+         -h1 agreement -h2 background -b $BEDTOOLS
+}
 
 
-perl ${METILENE_INPUT} -in1 "$(basename $fn .tsv)"_scored.bed_metilene.input \
-     -in2  "$(basename $fn .tsv)"_meth.bed_metilene.input \
-     -out "$(basename $fn .tsv)"_metilene \
-     -h1 agreement -h2 methylation -b $BEDTOOLS
+function call_metilene {
+    agreement_tag="$1"
+    background_tag="$2"
+    metilene_input="$3"
+    
+    $METILENE -t $NTHREADS -d 0.1 -m 10 -a "$agreement_tag" -b "$background_tag" \
+              "$metilene_input" > no_filtering.file
 
+}
 
-$METILENE -t $NTHREADS -d 0.1 -m 10 -a agreement -b methylation \
-           "$(basename $fn .tsv)"_metilene > no_filtering.file
+sample=sorjuela
+bam="$sample".bam
 
+run_methtuple "$bam"
 
+fn="$sample".CG.2.tsv
 
+get_agreement_score $fn
+get_methylation $fn
+
+prepare_metilene_input "$sample"_scored.bed "$sample"_meth.bed "$sample"
+
+call_metilene agreement methylation "$sample"_metilene
