c9c0bd2f0adf3fe6cb6c4c505fa4830f22636869
Izaskun Mallona <izaskun.mallona@gmail.com>
2019-06-27 14:40:37 +0200

    Add correlation axes and beta vs entropy plots

--- scripts/intersect_to_correlate.R
+++ scripts/intersect_to_correlate.R
@@ -42,7 +42,7 @@ annotation <- c( 'IMR90', 'HeLa-S3', 'HeLa-S3', 'GM23248', 'GM23248')
 
  ## bottom, left, top and right
 
-CairoPNG('correlation_encode_std_entropies.png', width= 600, height = 600)
+CairoPNG('correlation_encode_entropies.png', width= 600, height = 600)
 
 par(mfrow = c(5,5),
     mai = c(0.01, 0.01, 0.01, 0.01),
@@ -60,15 +60,19 @@ for (x in 1:length(samples)) {
         print(sprintf('i = %s, x= %s, y = %s, idx = %s', i, x, y, idx))
         if (x == y) {
             ## plot(1)
-            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
-            text(x = 1, y = 1, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+            ## plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            ## text(x = 1, y = 1, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+
+            plot(curr[,1], curr[,3], type='p', xlab = '', ylab = '', col ='white')
+            
+            text(x = 0.7, y = 0.7, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
             
         } else if (y > x) {
            
             
             plot(curr[,1], curr[,3], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
                  pch = 19,
-                 col = add.alpha('black', 0.1),
+                 col = add.alpha('black', 0.05),
                  cex = 0.5)            
         } else {
             plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
@@ -83,9 +87,9 @@ for (x in 1:length(samples)) {
 }
 
 ## mtext("standardized entropy (stdH)", side = 1, line = -2, adj = 3.5, cex = 1.2)
-mtext("standardized entropy (stdH)", side = 1, line = -2, outer = TRUE,  cex = 1.2)
+mtext("Shannon's entropy (H)", side = 1, line = -2, outer = TRUE,  cex = 1.2)
 ## mtext("standardized entropy (stdH)", side = 2, line = 40, adj =40)
-mtext("standardized entropy (stdH)", side = 2, line = -2, outer = TRUE, cex = 1.2)
+mtext("Shannon's entropy (H)", side = 2, line = -2, outer = TRUE, cex = 1.2)
 
 dev.off()
 
@@ -109,15 +113,17 @@ for (x in 1:length(samples)) {
         print(sprintf('i = %s, x= %s, y = %s, idx = %s', i, x, y, idx))
         if (x == y) {
             ## plot(1)
-            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
-            text(x = 1, y = 1, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+            ## plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            plot(curr[,2], curr[,4], type='p', xlab = '', ylab = '', col ='white')
+            
+            text(x = 0.5, y = 0.5, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
             
         } else if (y > x) {
            
             
             plot(curr[,2], curr[,4], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
                  pch = 19,
-                 col = add.alpha('black', 0.1),
+                 col = add.alpha('black', 0.05),
                  cex = 0.5)            
         } else {
             plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
@@ -140,40 +146,57 @@ dev.off()
 
 
 
+## cross beta vs entropy across replicates
 
 
+CairoPNG('correlation_encode_betas_vs_entropies.png', width= 600, height = 600)
 
+par(mfrow = c(5,5),
+    mai = c(0.01, 0.01, 0.01, 0.01),
+    pty = 's',
+    mar=c(1, 1, 1, 1))
 
-stop('old stuff below')
+i <- 1
 
+for (x in 1:length(samples)) {
+    for (y in 1:length(samples)) {
+        idx = i %% length(samples)
 
-
-## entropies
-
-par(mfrow = c(5,5), mai = c(0.01, 0.01, 0.01, 0.01), pty = 's')
-for (x in  c(imr, gm, hela)) {
-    for (y in c(imr, gm, hela)) {
-        curr <- d[[grep(sprintf('.*%s.*%s.*', x, y), fns)]]
-        plot(curr[,1], curr[,3], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
-             pch = 21,
-             col = add.alpha('black', 0.2))
-        
+        curr <- d[[grep(sprintf('.*%s.*%s.*', samples[x], samples[y]), fns)]]
+       
+        print(sprintf('i = %s, x= %s, y = %s, idx = %s', i, x, y, idx))
+        if (x == y) {
+            ## plot(1)
+            ## plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            plot(x = curr[,1], y = curr[,2], type='p', xlab = '', ylab = '', col ='white')
+            
+            text(x = 0.7, y = 0.5, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+            
+        } else if (y > x) {
+           
+            
+            plot(curr[,1], curr[,2], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
+                 pch = 19,
+                 col = add.alpha('black', 0.05),
+                 cex = 0.5)            
+        } else {
+            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            text(x = 1, y = 1,
+                 sprintf('rho = %s\nn =  %s',
+                         round(cor(curr[,1], curr[,2], method = 'spearman'), 3),
+                         nrow(curr)),
+                 cex = 1.4)
+        }
+        i = i +1
     }
 }
 
-## methyaltions
+## mtext("standardized entropy (stdH)", side = 1, line = -2, adj = 3.5, cex = 1.2)
+mtext("Shannon's Entropy (H)", side = 1, line = -2, outer = TRUE,  cex = 1.2)
+## mtext("standardized entropy (stdH)", side = 2, line = 40, adj =40)
+mtext("DNA methylation (beta)", side = 2, line = -2, outer = TRUE, cex = 1.2)
+
+dev.off()
 
 
-par(mfrow = c(5,5), mai = c(0.01, 0.01, 0.01, 0.01), pty = 's')
-for (x in  c(imr, gm, hela)) {
-    for (y in c(imr, gm, hela)) {
-        curr <- d[[grep(sprintf('.*%s.*%s.*', x, y), fns)]]
-        plot(curr[,2], curr[,4], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
-             pch = 21,
-             col = add.alpha('black', 0.2))
-        
-    }
-}
 
-
-## foo <- do.call(rbind.data.frame, d)
