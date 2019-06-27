4eba1b261adf1a1d74a091419bc3528f3666633d
Izaskun Mallona <izaskun.mallona@gmail.com>
2019-06-27 13:52:36 +0200

    Add correlation plots
    Adding axis pending

--- scripts/intersect_to_correlate.R
+++ scripts/intersect_to_correlate.R
@@ -4,7 +4,11 @@
 ##
 ## requires intersect_to_correlate.sh to be run first
 
-library(ggplot2)
+## library(ggplot2)
+library(Cairo)
+
+
+add.alpha <- function(cols, alpha) rgb(t(col2rgb(cols)/255), alpha = alpha)
 
 fns <- list.files('.', pattern = 'intersect_*')
 
@@ -28,12 +32,120 @@ for (fn in fns) {
 ##         }
 ##     }
 ## }
-
-add.alpha <- function(cols, alpha) rgb(t(col2rgb(cols)/255), alpha = alpha)
-
 hela <-  c('RVP', 'VFH')
 gm <- c('ELB', 'RGH')
 imr <- 'TXF'
+
+
+samples <- c(imr, hela, gm)
+annotation <- c( 'IMR90', 'HeLa-S3', 'HeLa-S3', 'GM23248', 'GM23248')
+
+ ## bottom, left, top and right
+
+CairoPNG('correlation_encode_std_entropies.png', width= 600, height = 600)
+
+par(mfrow = c(5,5),
+    mai = c(0.01, 0.01, 0.01, 0.01),
+    pty = 's',
+    mar=c(1, 1, 1, 1))
+
+i <- 1
+
+for (x in 1:length(samples)) {
+    for (y in 1:length(samples)) {
+        idx = i %% length(samples)
+
+        curr <- d[[grep(sprintf('.*%s.*%s.*', samples[x], samples[y]), fns)]]
+       
+        print(sprintf('i = %s, x= %s, y = %s, idx = %s', i, x, y, idx))
+        if (x == y) {
+            ## plot(1)
+            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            text(x = 1, y = 1, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+            
+        } else if (y > x) {
+           
+            
+            plot(curr[,1], curr[,3], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
+                 pch = 19,
+                 col = add.alpha('black', 0.1),
+                 cex = 0.5)            
+        } else {
+            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            text(x = 1, y = 1,
+                 sprintf('rho = %s\nn =  %s',
+                         round(cor(curr[,1], curr[,3], method = 'spearman'), 3),
+                         nrow(curr)),
+                 cex = 1.4)
+        }
+        i = i +1
+    }
+}
+
+## mtext("standardized entropy (stdH)", side = 1, line = -2, adj = 3.5, cex = 1.2)
+mtext("standardized entropy (stdH)", side = 1, line = -2, outer = TRUE,  cex = 1.2)
+## mtext("standardized entropy (stdH)", side = 2, line = 40, adj =40)
+mtext("standardized entropy (stdH)", side = 2, line = -2, outer = TRUE, cex = 1.2)
+
+dev.off()
+
+
+
+CairoPNG('correlation_encode_betas.png', width= 600, height = 600)
+
+par(mfrow = c(5,5),
+    mai = c(0.01, 0.01, 0.01, 0.01),
+    pty = 's',
+    mar=c(1, 1, 1, 1))
+
+i <- 1
+
+for (x in 1:length(samples)) {
+    for (y in 1:length(samples)) {
+        idx = i %% length(samples)
+
+        curr <- d[[grep(sprintf('.*%s.*%s.*', samples[x], samples[y]), fns)]]
+       
+        print(sprintf('i = %s, x= %s, y = %s, idx = %s', i, x, y, idx))
+        if (x == y) {
+            ## plot(1)
+            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            text(x = 1, y = 1, sprintf('%s\n%s', annotation[x],samples[x]), cex = 1.4)
+            
+        } else if (y > x) {
+           
+            
+            plot(curr[,2], curr[,4], axes=FALSE, frame.plot=TRUE, xlab = '', ylab = '',
+                 pch = 19,
+                 col = add.alpha('black', 0.1),
+                 cex = 0.5)            
+        } else {
+            plot(0,type='n',axes=FALSE,ann=FALSE, xlim = c(0,2), ylim = c(0,2))
+            text(x = 1, y = 1,
+                 sprintf('rho = %s\nn =  %s',
+                         round(cor(curr[,2], curr[,4], method = 'spearman'), 3),
+                         nrow(curr)),
+                 cex = 1.4)
+        }
+        i = i +1
+    }
+}
+
+## mtext("standardized entropy (stdH)", side = 1, line = -2, adj = 3.5, cex = 1.2)
+mtext("DNA methylation (beta)", side = 1, line = -2, outer = TRUE,  cex = 1.2)
+## mtext("standardized entropy (stdH)", side = 2, line = 40, adj =40)
+mtext("DNA methylation (beta)", side = 2, line = -2, outer = TRUE, cex = 1.2)
+
+dev.off()
+
+
+
+
+
+
+
+stop('old stuff below')
+
 
 
 ## entropies
