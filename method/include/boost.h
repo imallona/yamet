b721c71fde21cea6bd03812ac844f58c8c8365c3
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-12-07 03:27:08 +0100

    boost config for verbose printing of regions and ref

--- method/include/boost.h
+++ method/include/boost.h
@@ -5,7 +5,6 @@
 boost::program_options::variables_map parseCommandLine(int argc, char **argv);
 std::vector<std::string>              getTsvFiles(const boost::program_options::variables_map &vm);
 std::string                           getBed(const boost::program_options::variables_map &vm);
-std::string                           getChr(const boost::program_options::variables_map &vm);
 std::string                           getRef(const boost::program_options::variables_map &vm);
 std::string                           getDetOut(const boost::program_options::variables_map &vm);
 std::string                           getOut(const boost::program_options::variables_map &vm);
