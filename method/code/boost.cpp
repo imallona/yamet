5495d71aa7d08b8c06d63cebaba7b9d6a5d6319c
Atreya Choudhury <atreyachoudhury@hotmail.com>
2024-12-07 03:27:08 +0100

    boost config for verbose printing of regions and ref

--- method/code/boost.cpp
+++ method/code/boost.cpp
@@ -8,24 +8,30 @@ namespace po = boost::program_options;
 po::variables_map parseCommandLine(int argc, char **argv) {
   po::variables_map vm;
 
-  po::options_description desc("yamet - allowed options");
-  desc.add_options()("help,h", "produce help message")(
+  po::options_description gen("general");
+  gen.add_options()("help,h", "produce help message")(
       "tsv,t", po::value<std::vector<std::string>>()->composing(),
       ".tsv files for different cells")("ref,r", po::value<std::string>(),
                                         "path to tsv.gz file for reference CpG sites")(
       "bed,b", po::value<std::string>(), "path to bed file for regions of interest")(
-      "chr", po::value<std::string>(), "(optional) chr of interest")(
       "det-out,d", po::value<std::string>(), "(optional) path to detailed output file")(
       "out,o", po::value<std::string>(), "(optional) path to simple output file");
+
+  po::options_description ver("verbose");
+  ver.add_options()("print-bed", "print parsed regions file")("print-ref",
+                                                              "print parsed reference file");
+
+  po::options_description all;
+  all.add(gen).add(ver);
 
   po::positional_options_description p;
   p.add("tsv", -1);
 
-  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
   po::notify(vm);
 
   if (vm.count("help")) {
-    std::cout << desc << std::endl;
+    std::cout << "yamet" << std::endl << all << std::endl;
     throw std::runtime_error("Help message displayed");
   }
   return vm;
@@ -33,10 +39,6 @@ po::variables_map parseCommandLine(int argc, char **argv) {
 
 std::vector<std::string> getTsvFiles(const po::variables_map &vm) {
   return vm["tsv"].as<std::vector<std::string>>();
-}
-
-std::string getChr(const po::variables_map &vm) {
-  return vm["chr"].as<std::string>();
 }
 
 std::string getBed(const po::variables_map &vm) {
