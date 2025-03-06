7d90305b74bd4509e862b5ba141386b0fb611cd5
Atreya Choudhury <atreyachoudhury@hotmail.com>
2025-03-06 15:00:14 +0100

    mm10 genome sizes keep relevant

--- workflow/rules/src/download_mm10_genome_sizes.sh
+++ workflow/rules/src/download_mm10_genome_sizes.sh
@@ -5,4 +5,6 @@
 ## Izaskun Mallona
 
 mysql --user=genome --host=genome-mysql.soe.ucsc.edu -N -s -e \
-      'SELECT chrom,size FROM mm10.chromInfo' >${snakemake_output[0]}
+      'SELECT chrom,size FROM mm10.chromInfo' |
+      grep -E -w "chr[0-9XY]{1,2}" |
+      sort -k1,1 >${snakemake_output[0]}
