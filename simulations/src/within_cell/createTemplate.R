6645a3168646441960afb0e21e6a260172b4368b
Atreya Choudhury <atreyachoudhury@hotmail.com>
2025-05-02 16:05:53 +0200

    within cell strategy discussion

--- simulations/src/within_cell/createTemplate.R
+++ simulations/src/within_cell/createTemplate.R
@@ -24,11 +24,11 @@ write.table(
 chr <- rep("chrSim", features.N)
 lbound <- seq(0, by = features.length, length.out = features.N)
 ubound <- lbound + features.length
-het <- sample(1:10, size = features.N, replace = TRUE)
+vi <- sample(1:10, size = features.N, replace = TRUE)
 
 snips.length <- (features.length - 1) / 8
 
-snip_pos <- sapply(het, function(x) {
+snip_pos <- sapply(vi, function(x) {
   shuf_count <- floor(x * snips.length / (1.5 * 10))
   paste(sort(c(
     sample(1:snips.length, shuf_count, replace = F),
@@ -39,7 +39,7 @@ snip_pos <- sapply(het, function(x) {
 write.table(
   data.frame(
     chr = chr, lbound = lbound, ubound = ubound,
-    het = het, snip_pos = snip_pos
+    vi = vi, snip_pos = snip_pos
   ),
   out,
   sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE
